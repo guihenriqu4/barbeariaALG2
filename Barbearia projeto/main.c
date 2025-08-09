c9c60fbf231a9af08de7326a1fbf638b373dbe60
Emerson <gcintra0612@gmail.com>
2025-08-09 15:03:22 -0300

    Menu

--- Barbearia projeto/main.c
+++ Barbearia projeto/main.c
@@ -1,8 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void menuPrincipal() {
+    int opcao;
+
+    do {
+        printf("\n-- Sistema --\n");
+        printf("1. Cadastrar cliente\n");
+        printf("2. Listar clientes\n");
+        printf("3. Alterar clientes");
+        printf("4. Remover cliente\n");
+        printf("5. Sair\n");
+        printf("Escolha uma opcao: ");
+        scanf("%d", &opcao);
+
+        switch(opcao) {
+        case 1:
+            cadastrarCliente();
+            break;
+        case 2:
+            listarClientes();
+        case 3:
+            alterarClientes();
+        case 4:
+            removerClientes();
+        case 5:
+            printf("Saindo...\n");
+            break;
+        default:
+            printf("Opcao invalida, tente novamente.\n");
+
+        }
+    } while (opcao!=5);
+
+}
+
 int main()
 {
-    printf("Hello world!\n");
+    menuPrincipal();
     return 0;
 }
