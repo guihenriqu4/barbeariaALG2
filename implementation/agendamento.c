ca365a9d4ab85e7b5f9732660ffa5f217bb5bfbd
Guilherme <guilhermeconegundes5000@gmail.com>
2025-09-02 10:54:27 -0300

    Criação da função buscarAgendamentosPorCPF

--- implementation/agendamento.c
+++ implementation/agendamento.c
@@ -1,14 +1,36 @@
-// #include "../headers/agendamento.h"
-// #include "cliente.h"
-// #include "colab.h"
-// #include <stdio.h>
-// #include <stdlib.h>
+#include "../headers/agendamento.h"
+#include "cliente.h"
+#include "colab.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 // #define INCREMENTO 5
 
-// long int qtdAgen = 0;
-// long int capacidadeA = 0;
-// long int id = 1;
+long int qtdAgen = 0;
+long int capacidadeA = 0;
+long int id = 1;
+
+
+// CPF do cliente, retornando: Agendamentos ativos, nome e celular vinculados a esse CPF.
+void buscarAgendamentosPorCPF(Agendamento *agendamentos, int qtd, const char *cpf) {
+    int encontrou = 0;
+    for (int i = 0; i < qtd; i++) {
+        if (strcmp(agendamentos[i].cpfClinte, cpf) == 0) {
+            printf("Agendamento ID: %ld\n", agendamentos[i].id);
+            printf("ID do Colaborador: %d\n", agendamentos[i].idColab);
+            printf("Data: %s\n", agendamentos[i].data);
+            printf("Horário: %s\n", agendamentos[i].horario);
+            printf("Serviços desejados:\n");
+            for (int j = 0; agendamentos[i].servicoDesejado[j] != NULL; j++)
+                printf("- %s\n", agendamentos[i].servicoDesejado[j]);
+            printf("\n");
+            encontrou = 1;
+        }
+    }
+    if (!encontrou) {
+        printf("Nenhum agendamento encontrado para o CPF %s.\n", cpf);
+    }
+}
 
 
 // //Cadastrando novo agendamento
@@ -62,79 +84,6 @@
 //     qtdAgen++;
 // }
 
-// //função para converter a data do formato dd/MM/YY para um inteiro
-// int converterData(const char *data) {
-//     return (data[6] - '0') * 10000 +
-//            (data[7] - '0') * 1000 +
-//            (data[3] - '0') * 100 +
-//            (data[4] - '0') * 10 +
-//            (data[0] - '0') * 1 +
-//            (data[1] - '0');
-// }
-
-// //Listar todos os Agendamentos pos data informada, recebendo um cliente, colaborador e data base especificos
-// void listarAgendamentosPosData(Agendamento *agendamentos, int qtdAg, Cliente *clientes, int qtdCl, Colab *colabs, int qtdCol) {
-//     if (qtdAg == 0) {
-//         printf("Nenhum agendamento realizado.\n");
-//         return;
-//     }
-
-//     int idColab;
-//     char cpf[14], dataBase[9];
-
-//     printf("Digite o ID do colaborador: ");
-//     scanf("%d", &idColab);
-//     getchar();
-
-//     printf("Digite o CPF do cliente (000.000.000-00): ");
-//     scanf(" %13[^\n]", cpf);
-
-//     printf("Digite a data base (dd/MM/YY), para informar agendamentos subsequentes: ");
-//     scanf(" %[^\n]", dataBase);
-
-//     int dataCmp = converterData(dataBase);
-//     int encontrados = 0;
-// <<<<<<< HEAD
-
-//     printf("\nTodos os agendamentos do colaborador ID %d e cliente CPF %s após a data %s:\n\n", idColab, cpf, dataBase);
-// =======
-
-//     printf("\nTodos os agendamentos do colaborador ID %d e cliente CPF %s apos a data %s:\n\n", idColab, cpf, dataBase);
-// >>>>>>> 41d0ce020617a9417b24936d3b1666ef3df6c185
-
-//     for (int i = 0; i < qtdAg; i++) {
-//         if (strcmp(agendamentos[i].cpfClinte, cpf) == 0 && agendamentos[i].idColab == idColab) {
-
-//             int dataAg = converterData(agendamentos[i].data);
-
-//             if (dataAg > dataCmp) {
-//                 encontrados++;
-
-//                 printf("\n--- Agendamento %ld ---\n", agendamentos[i].id);
-//                 printf("Data: %s\n", agendamentos[i].data);
-// <<<<<<< HEAD
-//                 printf("Horário: %s\n", agendamentos[i].horario);
-
-//                 //mantendo por boa pratica, mas no caso nao sei se precisa
-//                 if (cli == NULL)
-//                     printf("Cliente: Informação não encontrada\n");
-// =======
-//                 printf("Horario: %s\n", agendamentos[i].horario);
-// >>>>>>> 41d0ce020617a9417b24936d3b1666ef3df6c185
-
-
-//                 printf("Serviços desejados:\n");
-//                 for (int j = 0; agendamentos[i].servicoDesejado[j] != NULL; j++) {
-//                     printf("- %s\n", agendamentos[i].servicoDesejado[j]);
-//                 }
-//             }
-//         }
-//     }
-
-//     if (encontrados == 0) {
-//         printf("\nNenhum agendamento encontrado para os filtros informados.\n");
-//     }
-// }
 
 // //Listando todos os Agendamentos "Nao obrigatorio"
 // /*
